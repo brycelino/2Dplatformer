cbf4c024a93b09b38c5fc27871da44f937d9bf17
brycelino <brycejeff16@gmail.com>
2022-04-29 03:12:44 -0500

    2D platformer in Unity
    You'll need to create a player character with adjustable speed and jump height, platforms to move and jump across, as well as some coins to collect. If the player collects all the coins, they win the game and should see the ending screen.
    
    If the player falls and goes below the screen, they should lose a life and respawn at the beginning (but they should not lose any coins). If the player runs out of lives, they should be sent to a Game Over screen. The Game Over screen should allow the player to return to the title screen.
    
    You need to have at least 4 scenes: Title, Gameplay, Game Over, and Ending/Victory.

---
+++ Platformer/Assets/Scripts/Physicstest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physicstest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collison Happened.");
+        Debug.Log("Collided wuth: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered " + collision.gameObject.name);
+    }
+}
---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    public float jumpForce = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        //transform.Translate(Vector3.right * h * Time.deltaTime * speed);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        Vector3 vel = new Vector3();
+
+        vel.x = h * speed;
+        vel.y = rb.velocity.y;
+        vel.z = 0;
+
+        rb.velocity = vel;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //rb.AddForce(Vector2.up * jumpForce);
+            vel.y = jumpForce;
+        }
+
+        rb.velocity = vel;
+    }
+
+    
+}
